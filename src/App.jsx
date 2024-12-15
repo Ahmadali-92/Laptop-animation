b05d84e811a58801564a52f6c15aeacb4a65acba
Ahmad Ali <sheikh.ahmad.ali92@gmail.com>
2024-12-15 20:20:33 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import {Canvas} from '@react-three/fiber';
+import React from 'react';
+import './style.css';
+import {Environment, ScrollControls} from '@react-three/drei';
+import MacContainer from './MacContainer';
+
+const App = () => {
+  return (
+    <Canvas camera={{fov: 12, position: [0, -10, 220]}}>
+      <Environment
+        files={[
+          'https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/2k/studio_small_09_2k.hdr',
+        ]}
+      />
+      <ScrollControls>
+        <MacContainer />
+      </ScrollControls>
+    </Canvas>
+  );
+};
+
+export default App;
